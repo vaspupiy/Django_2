571a66cc497a08c10e181caff661767ac9f3a386
Vasiliy <vaspupiy@mail.ru>
2021-04-11 22:31:10 +0300

    lesson_3_add_cbv

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -5,9 +5,14 @@ from basketapp import views as basketapp
 app_name = 'basketapp'
 
 urlpatterns = [
-    path('', basketapp.basket, name='view'),
-    path('add/<int:pk>/', basketapp.basket_add, name='add'),
-    path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
-    path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit'),
+    # path('', basketapp.basket, name='view'),
+    path('', basketapp.BasketListView.as_view(), name='view'),
+    # path('add/<int:pk>/', basketapp.basket_add, name='add'),
+    # path('add/<int:pk>/', basketapp.basket_add, name='add'),
+    path('add/<int:pk>/', basketapp.BasketAddView.as_view(), name='add'),
+    # path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
+    path('remove/<int:pk>/', basketapp.BasketDeleteView.as_view(), name='remove'),
+    # path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit'),
+    path('edit/<int:pk>/<int:quantity>/', basketapp.BasketUpdateView.as_view(), name='edit'),
 ]
 
