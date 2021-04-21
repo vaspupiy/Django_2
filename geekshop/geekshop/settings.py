815a313a096f18014f481151122061c26a189f02
Vasiliy <vaspupiy@mail.ru>
2021-04-21 22:11:32 +0300

    lesson_6_10

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'r(s=@b^f@3ov9s12^#_9#ym%le#ww7h%)^86835acq3k#syqct'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['5.63.157.29']
