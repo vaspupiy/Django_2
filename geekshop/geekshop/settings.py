61c7c2e80aad44482b3034ada2219f0578e91ca3
Vasiliy <vaspupiy@mail.ru>
2021-04-21 23:27:45 +0300

    lesson_6_14

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -113,7 +113,7 @@ if DEBUG:
         'debug_toolbar.panels.logging.LoggingPanel',
         'debug_toolbar.panels.redirects.RedirectsPanel',
         'debug_toolbar.panels.profiling.ProfilingPanel',
-        # 'template_profiler_panel.panels.template.TemplateProfilerPanel',
+        'template_profiler_panel.panels.template.TemplateProfilerPanel',
     ]
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
--- geekshop/mainapp/context_processor.py
+++ geekshop/mainapp/context_processor.py
@@ -4,7 +4,8 @@ from basketapp.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = Basket.objects.filter(user=request.user)
+        # basket_list = Basket.objects.filter(user=request.user)
+        basket_list = request.user.basket.select_related()
     return {
-        'basket': basket_list
+        'basket': basket_list,
     }
