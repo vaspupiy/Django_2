0ea1ed83ebdefc9756d49fd6d3822bf5554d27c2
Vasiliy <vaspupiy@mail.ru>
2021-04-21 12:55:37 +0300

    lesson_6_6

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -168,17 +168,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
-
-MEDIA_URL = '/media/'
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_ROOT = (
-#         BASE_DIR / 'media'
-# )
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = (
+        BASE_DIR / 'media'
+)
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
 
