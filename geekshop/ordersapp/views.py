657e0daf0b5ca27f1c8588c4c32610d81067be6f
Vasiliy <vaspupiy@mail.ru>
2021-04-23 23:56:27 +0300

    lesson_6_24

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -80,7 +80,9 @@ class OrderUpdateView(UpdateView):
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
 
         if self.request.POST:
-            formset = OrderFormSet(self.request.POST, instance=self.object)
+            queryset = self.object.orderitems.select_realated()
+            # formset = OrderFormSet(self.request.POST, instance=self.object)
+            formset = OrderFormSet(instance=self.object, queryset=queryset)
         else:
             formset = OrderFormSet(instance=self.object)
             for form in formset.forms:
