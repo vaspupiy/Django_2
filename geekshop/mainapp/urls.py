0c2f5e91d603dac5695813220112d9a822b18c44
Vasiliy <vaspupiy@mail.ru>
2021-04-25 22:12:22 +0300

    lesson_7_13

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -1,15 +1,13 @@
-
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 import mainapp.views as mainapp
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
-    path('<int:pk>/', mainapp.products, name='category'),
+    path('<int:pk>/', cache_page(360)(mainapp.products), name='category'),
     # path('<int:pk>/<int:page>', mainapp.products, name='page'),
     path('product/<int:pk>/', mainapp.product, name='product')
 ]
-
-
-
